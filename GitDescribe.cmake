de25c39d9ef3809fed15df39c679dc665396c440
Robin Degen <robindegen@gmail.com>
2015-03-02 15:01:21 +0100

    Fixed working directory for git describe and added additional logging when the git process execution does go wrong.

--- GitDescribe.cmake
+++ GitDescribe.cmake
@@ -5,13 +5,15 @@ function(get_git_describe_tag git_describe_tag)
 
     execute_process(
         COMMAND ${GIT_EXECUTABLE} "describe" "--tags"
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
         OUTPUT_VARIABLE GIT_DESCRIBE_OUTPUT
+        ERROR_VARIABLE GIT_DESCRIBE_ERROR_OUTPUT
         RESULT_VARIABLE GIT_DESCRIBE_RESULT
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
     if (NOT GIT_DESCRIBE_RESULT EQUAL 0)
-        message(FATAL_ERROR "Could not parse git describe version.")
+        message(FATAL_ERROR "Could not parse git describe version.\nOutput was ${GIT_DESCRIBE_ERROR_OUTPUT}")
     endif()
 
     set(${git_describe_tag} ${GIT_DESCRIBE_OUTPUT} PARENT_SCOPE)
