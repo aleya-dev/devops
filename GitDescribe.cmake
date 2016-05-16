94cef7255485a37e2c60f528e69db0043cfdc6da
Robin Degen <robindegen@gmail.com>
2016-05-16 19:03:51 +0200

    Added copyright headers.

--- GitDescribe.cmake
+++ GitDescribe.cmake
@@ -1,3 +1,16 @@
+# ROBIN DEGEN; CONFIDENTIAL
+#
+# 2012 - 2016 Robin Degen
+# All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains the property of
+# Robin Degen and its suppliers, if any. The intellectual and technical
+# concepts contained herein are proprietary to Robin Degen and its suppliers
+# and may be covered by U.S. and Foreign Patents, patents in process, and are
+# protected by trade secret or copyright law. Dissemination of this
+# information or reproduction of this material is strictly forbidden unless
+# prior written permission is obtained from Robin Degen.
+
 function(get_git_describe_tag git_describe_tag)
     if (NOT GIT_FOUND)
         find_package(Git REQUIRED)
