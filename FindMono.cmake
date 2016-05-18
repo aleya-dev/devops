0a42f3c000e91562c68bce4bb8c352f62ba57efb
Robin Degen <robindegen@gmail.com>
2016-05-18 18:17:51 +0200

    Cleaned up the mono findscript and fixed finding mcs on Unix.

--- FindMono.cmake
+++ FindMono.cmake
@@ -17,12 +17,7 @@ find_program(
 )
 
 find_program(
-    MCS_EXECUTABLE mcs.bat mcs
-    PATH $ENV{MONO_DEPENDENCIES_PREFIX}/bin
-)
-
-find_program(
-    GACUTIL_EXECUTABLE gacutil
+    MONO_MCS_EXECUTABLE mcs.bat NAMES mcs
     PATH $ENV{MONO_DEPENDENCIES_PREFIX}/bin
 )
 
@@ -33,14 +28,8 @@ find_library(
 
 set(MONO_FOUND FALSE CACHE INTERNAL "")
 
-if(MONO_EXECUTABLE AND MCS_EXECUTABLE AND GACUTIL_EXECUTABLE AND MONO_MAIN_LIBRARY)
+if(MONO_EXECUTABLE AND MONO_MCS_EXECUTABLE AND MONO_MAIN_LIBRARY)
     set(MONO_FOUND TRUE CACHE INTERNAL "")
-
-    # default GAC is located in <prefix>/lib/mono/gac
-    find_path(MONO_GAC_PREFIX lib/mono/gac PATHS "/usr;/usr/local")
-
-    set(MONO_GAC_PREFIX "${MONO_GAC_PREFIX}" CACHE PATH "Mono GAC prefix")
-    set(MONO_GAC_DIR "${MONO_GAC_PREFIX}/lib/mono" CACHE PATH "Mono GAC directory")
 
     get_filename_component(MONO_LIBRARY_PATH "${MONO_MAIN_LIBRARY}" DIRECTORY)
     get_filename_component(MONO_INCLUDE_PATH "${MONO_LIBRARY_PATH}/../include/mono-2.0" ABSOLUTE)
@@ -52,6 +41,8 @@ if(MONO_EXECUTABLE AND MCS_EXECUTABLE AND GACUTIL_EXECUTABLE AND MONO_MAIN_LIBRA
         set(MONO_LIBRARIES "${MONO_LIBRARIES};${CORE_FOUNDATION_LIBRARY}")
     endif ()
 
-    execute_process(COMMAND ${MCS_EXECUTABLE} --version OUTPUT_VARIABLE MONO_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(COMMAND ${MONO_MCS_EXECUTABLE} --version OUTPUT_VARIABLE MONO_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
     string(REGEX REPLACE ".*version ([^ ]+)" "\\1" MONO_VERSION "${MONO_VERSION}")
+    message(STATUS "Found Mono version: ${MONO_MAIN_LIBRARY} (found version \"${MONO_VERSION}\")")
 endif()
+
