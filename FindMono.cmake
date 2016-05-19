f69013ca88703526fec7c2f44c7768c27e0c71ad
Robin Degen <robindegen@gmail.com>
2016-05-19 08:06:51 +0200

    Added various cache variables to FindMono Automaticly copy mono-2.0.dll to the binary folder on Windows.

--- FindMono.cmake
+++ FindMono.cmake
@@ -17,7 +17,7 @@ find_program(
 )
 
 find_program(
-    MONO_MCS_EXECUTABLE mcs.bat NAMES mcs
+    MONO_MCS_EXECUTABLE mcs
     PATH $ENV{MONO_DEPENDENCIES_PREFIX}/bin
 )
 
@@ -32,13 +32,29 @@ if(MONO_EXECUTABLE AND MONO_MCS_EXECUTABLE AND MONO_MAIN_LIBRARY)
     set(MONO_FOUND TRUE CACHE INTERNAL "")
 
     get_filename_component(MONO_LIBRARY_PATH "${MONO_MAIN_LIBRARY}" DIRECTORY)
-    get_filename_component(MONO_INCLUDE_PATH "${MONO_LIBRARY_PATH}/../include/mono-2.0" ABSOLUTE)
+    set(MONO_LIBRARY_PATH "${MONO_LIBRARY_PATH}" CACHE PATH "")
 
-    set(MONO_LIBRARIES "${MONO_MAIN_LIBRARY}")
-    
+    get_filename_component(MONO_INCLUDE_PATH "${MONO_LIBRARY_PATH}/../include/mono-2.0" ABSOLUTE)
+    set(MONO_INCLUDE_PATH "${MONO_INCLUDE_PATH}" CACHE PATH "")
+    set(MONO_ASSEMBLY_PATH "${MONO_LIBRARY_PATH}" CACHE PATH "")
+
+    get_filename_component(MONO_CONFIG_PATH "${MONO_LIBRARY_PATH}/../etc" ABSOLUTE)
+    set(MONO_CONFIG_PATH "${MONO_CONFIG_PATH}" CACHE PATH "")
+
+    get_filename_component(MONO_BINARY_PATH "${MONO_LIBRARY_PATH}/../bin" ABSOLUTE)
+    set(MONO_BINARY_PATH "${MONO_BINARY_PATH}" CACHE PATH "")
+
+    set(MONO_LIBRARIES "${MONO_MAIN_LIBRARY}" CACHE STRING "")
+
     if (APPLE)
         find_library(CORE_FOUNDATION_LIBRARY CoreFoundation)
         set(MONO_LIBRARIES "${MONO_LIBRARIES};${CORE_FOUNDATION_LIBRARY}")
+    endif ()
+
+    if (MSVC)
+        include(CopyToRuntimePath)
+        find_file(MONO_DLL_PATH mono-2.0.dll PATH ${MONO_BINARY_PATH})
+        copy_files_to_runtime_path(FILES ${MONO_DLL_PATH})
     endif ()
 
     execute_process(COMMAND ${MONO_MCS_EXECUTABLE} --version OUTPUT_VARIABLE MONO_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
