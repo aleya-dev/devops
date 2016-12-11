25f7441ef8d16d805ef58f4fc2c126487c012f59
Robin Degen <robindegen@gmail.com>
2016-12-11 18:07:17 +0100

    Added experimental Aeon Engine find script. Only works on windows currently.

---
+++ FindAeonEngine.cmake
@@ -0,0 +1,80 @@
+# ROBIN DEGEN
+#
+# 2012 - 2016 Robin Degen
+# All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains the property of
+# Robin Degen and its suppliers, if any. The intellectual and technical
+# concepts contained herein are proprietary to Robin Degen and its suppliers
+# and may be covered by U.S. and Foreign Patents, patents in process, and are
+# protected by trade secret or copyright law. Dissemination of this
+# information or reproduction of this material is strictly forbidden unless
+# prior written permission is obtained from Robin Degen.
+
+find_package(OpenGL REQUIRED)
+
+if (APPLE)
+    message(STATUS "Building on Apple. Finding Frameworks.")
+    find_library(COCOA_LIBRARY Cocoa)
+    find_library(IOKIT_LIBRARY IOKit)
+    find_library(COREVIDEO_LIBRARY CoreVideo)
+    mark_as_advanced(COCOA_LIBRARY IOKIT_LIBRARY COREVIDEO_LIBRARY)
+
+    message(STATUS "Cocoa: ${COCOA_LIBRARY}")
+    message(STATUS "IOKit: ${IOKIT_LIBRARY}")
+    message(STATUS "CoreVideo: ${COREVIDEO_LIBRARY}")
+    target_link_libraries(AeonEngineTemplate ${COCOA_LIBRARY} ${IOKIT_LIBRARY} ${COREVIDEO_LIBRARY})
+endif ()
+
+add_library(Aeon::Engine STATIC IMPORTED)
+set_target_properties(Aeon::Engine PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${AEON_ENGINE_PATH}/include)
+
+if (MSVC)
+    set_target_properties(Aeon::Engine PROPERTIES IMPORTED_LOCATION_DEBUG "${AEON_ENGINE_PATH}/lib/aeon_engine_d.lib")
+    set_target_properties(Aeon::Engine PROPERTIES IMPORTED_LOCATION_RELEASE "${AEON_ENGINE_PATH}/lib/aeon_engine.lib")
+
+    set(AEON_LIBRARIES "")
+    list(APPEND AEON_LIBRARIES opengl32.lib)
+
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_assets_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_basic_scene_manager_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_common_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_data_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_gfx_core_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_gfx_gl_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_gfx_gl_common_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_input_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_platform_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_resources_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_scene_core_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/aeon_engine_storage_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/assimp-vc140-mt_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/freetype_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/glfw3_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/libglew32_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/libpng_static_d.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:Debug>:${AEON_ENGINE_PATH}/lib/zlibstatic_d.lib>")
+
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_assets.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_basic_scene_manager.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_common.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_data.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_gfx_core.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_gfx_gl.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_gfx_gl_common.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_input.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_platform.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_resources.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_scene_core.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/aeon_engine_storage.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/assimp-vc140-mt.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/freetype.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/glfw3.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/libglew32.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/libpng_static.lib>")
+    list(APPEND AEON_LIBRARIES "$<$<CONFIG:RELEASE>:${AEON_ENGINE_PATH}/lib/zlibstatic.lib>")
+
+    set_target_properties(Aeon::Engine PROPERTIES INTERFACE_LINK_LIBRARIES "${AEON_LIBRARIES}")
+endif ()
