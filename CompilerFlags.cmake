6c3bc18f382f16a57c1fa022f6eafc0b774a0281
Robin Degen <robindegen@gmail.com>
2017-11-09 10:42:55 +0100

    Added /permissive- to enforce ISO c++ in Visual Studio.

--- CompilerFlags.cmake
+++ CompilerFlags.cmake
@@ -34,8 +34,9 @@ if (MSVC)
 
         message("Visual Studio detected. Setting flags:")
         message(" - Treat warnings as errors (/WX)")
+        message(" - Enforce latest C++17 ISO standard.")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++latest")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++latest /permissive-")
     endif ()
 
     message(" - Defining _SCL_SECURE_NO_WARNINGS")
