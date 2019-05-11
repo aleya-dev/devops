8ac8d5c134ad3680d2e9187ad22d1d6c61ac74ee
Robin Degen <robindegen@gmail.com>
2019-05-11 20:29:36 +0200

    Add find script for Google Mock

---
+++ FindGMock.cmake
@@ -0,0 +1,56 @@
+# Copyright (c) 2012-2019 Robin Degen
+
+find_path(GMock_INCLUDE_DIRS gmock/gmock.h
+    DOC
+        "Found gmock include directory"
+    ENV
+        GMOCK_ROOT
+    PATH_SUFFIXES
+        include
+)
+
+find_library(GMock_LIBRARIES
+    NAMES libgmock.so gmock
+    DOC "Found gmock library path"
+    ENV
+        GMOCK_ROOT
+    PATH_SUFFIXES
+        lib
+        lib64
+)
+
+find_library(GMock_MAIN_LIBRARIES
+    NAMES libgmock_main.so gmock_main
+    DOC "Found gmock main library path"
+    ENV
+        GMOCK_ROOT
+    PATH_SUFFIXES
+        lib
+        lib64
+)
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMock
+    FOUND_VAR
+        GMock_FOUND
+    REQUIRED_VARS
+        GMock_INCLUDE_DIRS
+        GMock_LIBRARIES
+        GMock_MAIN_LIBRARIES
+)
+
+if (GMock_FOUND)
+    if (NOT TARGET GTest::GMock)
+        add_library(GTest::GMock STATIC IMPORTED)
+        set_target_properties(GTest::GMock PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${GMock_INCLUDE_DIRS})
+        set_target_properties(GTest::GMock PROPERTIES IMPORTED_LOCATION ${GMock_LIBRARIES})
+    endif ()
+
+    if (NOT TARGET GTest::GmockMain)
+        add_library(GTest::GmockMain STATIC IMPORTED)
+        set_target_properties(GTest::GmockMain PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${GMock_INCLUDE_DIRS})
+        set_target_properties(GTest::GmockMain PROPERTIES IMPORTED_LOCATION ${GMock_MAIN_LIBRARIES})
+        set_target_properties(GTest::GmockMain PROPERTIES INTERFACE_LINK_LIBRARIES GTest::GMock)
+    endif ()
+endif ()
+
