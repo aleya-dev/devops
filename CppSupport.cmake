71a0db37da372c955d66a8d48c1bc71d2682cb8a
Robin Degen <robindegen@gmail.com>
2019-06-05 18:39:14 +0200

    Add compiler C++ support checks for AEON_COMPILER_HAS_FROM_CHARS_FLOAT and AEON_COMPILER_HAS_FROM_CHARS_LONG_DOUBLE

--- CppSupport.cmake
+++ CppSupport.cmake
@@ -28,6 +28,26 @@ check_cxx_source_compiles("
         }"
     AEON_COMPILER_HAS_FROM_CHARS_INTEGER)
 
+message(STATUS "Detecting support for float std::from_chars")
+
+check_cxx_source_compiles("
+        #include <charconv>
+        #include <string>
+
+        int main(int argc, char *argv[])
+        {
+            std::string str;
+
+            auto value = 0.0f;
+            const auto [ptr, ec] = std::from_chars(std::data(str), std::data(str) + std::size(str), value);
+
+            if (ec != std::errc{})
+                return 1;
+
+            return 0;
+        }"
+    AEON_COMPILER_HAS_FROM_CHARS_FLOAT)
+
 message(STATUS "Detecting support for double std::from_chars")
 
 check_cxx_source_compiles("
@@ -47,5 +67,26 @@ check_cxx_source_compiles("
             return 0;
         }"
     AEON_COMPILER_HAS_FROM_CHARS_DOUBLE)
+
+message(STATUS "Detecting support for long double std::from_chars")
+
+check_cxx_source_compiles("
+        #include <charconv>
+        #include <string>
+
+        int main(int argc, char *argv[])
+        {
+            std::string str;
+
+            long double value = 0.0;
+            const auto [ptr, ec] = std::from_chars(std::data(str), std::data(str) + std::size(str), value);
+
+            if (ec != std::errc{})
+                return 1;
+
+            return 0;
+        }"
+    AEON_COMPILER_HAS_FROM_CHARS_LONG_DOUBLE)
+
 
 configure_file(${CMAKE_CURRENT_LIST_DIR}/aeon_cpp_support.h.in aeon_cpp_support.h @ONLY)
