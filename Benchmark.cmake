0b42918b32025811a04c767ea12ffccca7411dcb
Robin Degen <robindegen@gmail.com>
2022-10-02 01:27:32 +0200

    Fix Benchmark.cmake to use the correct target name for google benchmark

--- Benchmark.cmake
+++ Benchmark.cmake
@@ -1,7 +1,7 @@
 # Distributed under the BSD 2-Clause License - Copyright 2012-2021 Robin Degen
 
-if (NOT TARGET GoogleBenchmark::GoogleBenchmark AND NOT TARGET CONAN_PKG::benchmark)
-    message(FATAL_ERROR "Benchmark.cmake requires Google Benchmark (GoogleBenchmark::GoogleBenchmark or CONAN_PKG::benchmark target is missing)")
+if (NOT TARGET benchmark::benchmark AND NOT TARGET CONAN_PKG::benchmark)
+    message(FATAL_ERROR "Benchmark.cmake requires Google Benchmark (benchmark::benchmark or CONAN_PKG::benchmark target is missing)")
 endif ()
 
 include(CMakeParseArguments)
@@ -41,10 +41,10 @@ function(add_benchmark_suite)
         target_include_directories(${BENCHMARK_PARSED_ARGS_TARGET} PRIVATE ${BENCHMARK_PARSED_ARGS_INCLUDES})
     endif ()
 
-    if (TARGET GoogleBenchmark::GoogleBenchmark)
+    if (TARGET benchmark::benchmark)
         target_link_libraries(
             ${BENCHMARK_PARSED_ARGS_TARGET}
-            GoogleBenchmark::GoogleBenchmark
+            benchmark::benchmark
         )
     else ()
         target_link_libraries(
