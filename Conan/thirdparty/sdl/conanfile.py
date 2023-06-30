cc1c94f2a57e34884e6db6131fc8c1e3e49ff002
Robin Degen <robindegen@gmail.com>
2023-06-30 18:27:32 +0200

    Add support for building shared libs to sdl

--- Conan/thirdparty/sdl/conanfile.py
+++ Conan/thirdparty/sdl/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain
-from conan.tools.files import rmdir
+from conan.tools.files import rmdir, rm
 import os
 
 
@@ -8,28 +8,36 @@ required_conan_version = ">=2.0"
 
 
 class SDL2Conan(ConanFile):
-    python_requires = "aleya-conan-base/1.0"
+    python_requires = "aleya-conan-base/1.0.1"
     python_requires_extend = "aleya-conan-base.AleyaCmakeBase"
 
     name = "sdl"
     git_repository = "https://github.com/aleya-dev/mirror-package-sdl.git"
     git_branch = "2.28.0"
 
-    def on_generate(self, tc: CMakeToolchain):
-        tc.variables["SDL_SHARED"] = False
-        tc.variables["SDL_STATIC"] = True
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["SDL_SHARED"] = self.options.shared
+        tc.variables["SDL_STATIC"] = not self.options.shared
+        tc.variables["SDL_TEST"] = False
+        tc.variables["SDL_TESTS"] = False
+        tc.generate()
 
     def on_package(self, cmake: CMake):
-        rmdir(self, os.path.join(self.package_folder, "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "aclocal"))
         rmdir(self, os.path.join(self.package_folder, "licenses"))
+        rm(self, "sdl2-config", os.path.join(self.package_folder, "bin"), recursive=False)
 
     def package_info(self):
         postfix = "d" if self.settings.os != "Android" and self.settings.build_type == "Debug" else ""
+        static_postfix = "-static" if not self.options.shared else ""
 
         self.cpp_info.set_property("cmake_file_name", "SDL2")
 
-        self.cpp_info.components["libsdl2"].libs = ["SDL2-static" + postfix]
+        self.cpp_info.components["libsdl2"].libs = ["SDL2" + static_postfix + postfix]
         self.cpp_info.components["libsdl2"].set_property("cmake_target_name", "SDL2::SDL2")
 
         self.cpp_info.components["sdl2main"].libs = ["SDL2main" + postfix]
