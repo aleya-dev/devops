d5af7894270cb7553011b2452d4b8d41a0c5d182
Robin Degen <robindegen@gmail.com>
2023-06-30 16:11:14 +0200

    Add support for building zlib as a shared library

--- Conan/thirdparty/zlib/conanfile.py
+++ Conan/thirdparty/zlib/conanfile.py
@@ -8,31 +8,31 @@ required_conan_version = ">=2.0"
 
 
 class ZlibConan(ConanFile):
-    python_requires = "aleya-conan-base/1.0"
+    python_requires = "aleya-conan-base/1.0.1"
     python_requires_extend = "aleya-conan-base.AleyaCmakeBase"
 
     name = "zlib"
     git_repository = "https://github.com/aleya-dev/mirror-package-zlib.git"
     git_branch = "1.2.13"
+    ignore_cpp_standard = True
 
     def on_generate(self, tc: CMakeToolchain):
-        tc.variables["BUILD_SHARED_LIBS"] = False
+        tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
         tc.variables["INSTALL_LIB_DIR"] = "lib"
         tc.variables["INSTALL_INC_DIR"] = "include"
 
     def on_package(self, cmake: CMake):
-        rmdir(self, os.path.join(self.package_folder, "bin"))
         rmdir(self, os.path.join(self.package_folder, "share"))
-        rm(self, "zlib.lib", os.path.join(self.package_folder, "lib"), recursive=True)
-        rm(self, "zlibd.lib", os.path.join(self.package_folder, "lib"), recursive=True)
+
+        if self.options.shared:
+            rm(self, "zlibstatic.lib", os.path.join(self.package_folder, "lib"), recursive=True)
+            rm(self, "zlibstaticd.lib", os.path.join(self.package_folder, "lib"), recursive=True)
+        else:
+            rmdir(self, os.path.join(self.package_folder, "bin"))
+            rm(self, "zlib.lib", os.path.join(self.package_folder, "lib"), recursive=True)
+            rm(self, "zlibd.lib", os.path.join(self.package_folder, "lib"), recursive=True)
 
     def package_info(self):
-        if self.settings.os == "Windows":
-            postfix = "d" if self.settings.build_type == "Debug" else ""
-            libname = "zlibstatic" + postfix
-        else:
-            libname = "z"
-
         self.cpp_info.set_property("cmake_find_mode", "both")
         self.cpp_info.set_property("cmake_file_name", "ZLIB")
         self.cpp_info.set_property("cmake_target_name", "ZLIB::ZLIB")
