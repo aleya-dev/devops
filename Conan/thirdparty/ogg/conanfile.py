2f7528f7ace38378e1d1d00c6566ee5ab5e8d890
Robin Degen <robindegen@gmail.com>
2023-07-05 17:17:21 +0200

    Add libogg conan package

---
+++ Conan/thirdparty/ogg/conanfile.py
@@ -0,0 +1,49 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain
+from conan.tools.files import rmdir, rm, collect_libs
+import os
+
+
+required_conan_version = ">=2.0"
+
+
+class OggConan(ConanFile):
+    python_requires = "aleya-conan-base/[>=1.1.0 <1.2.0]"
+    python_requires_extend = "aleya-conan-base.AleyaCmakeBase"
+
+    name = "ogg"
+    git_repository = "git@github.com:aleya-dev/mirror-package-ogg.git"
+    git_branch = "1.3.5"
+    ignore_cpp_standard = True
+
+    options = {
+        "shared": [False, True],
+        "fPIC": [False, True]
+    }
+
+    default_options = {
+        "shared": False,
+        "fPIC": True
+    }
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_TESTING"] = False
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
+        tc.generate()
+
+    def package(self):
+        super().package()
+
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_file_name", "Ogg")
+        self.cpp_info.set_property("cmake_target_name", "Ogg::ogg")
+        self.cpp_info.set_property("pkg_config_name", "ogg")
+
+        self.cpp_info.libs = collect_libs(self)
