3747d2d9979f858941a63f194fb7d2c2c98d4dfb
Robin Degen <robindegen@gmail.com>
2023-09-18 23:30:48 +0200

    Update the aleya conan base package in all thirdparty packages

--- Conan/thirdparty/sdl/conanfile.py
+++ Conan/thirdparty/sdl/conanfile.py
@@ -7,7 +7,7 @@ required_conan_version = ">=2.0"
 
 
 class SDL2Conan(ConanFile):
-    python_requires = "aleya-conan-base/[>=1.1.0 <1.2.0]"
+    python_requires = "aleya-conan-base/[>=1.2.0 <1.3.0]"
     python_requires_extend = "aleya-conan-base.AleyaConanBase"
 
     name = "sdl"
