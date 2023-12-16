e61daa2f29468d71cc1ade8266a58a8da993cbd8
Robin Degen <robindegen@gmail.com>
2023-12-16 16:26:06 +0100

    Update the Aleya Conan Base for the newly designed infra where the thirdparty conanfile lives in the same repository as the sources so it is much easier to maintain

--- Conan/aleya-conan-base/conanfile.py
+++ Conan/aleya-conan-base/conanfile.py
@@ -37,28 +37,11 @@ class AleyaConanBase:
     settings = "os", "arch", "compiler", "build_type"
 
     """
-    The git repository to use as source. If you do wish to use a different source, you must implement the
-    source() method yourself.
-    """
-    git_repository: str
-
-    """
-    The git branch to use as source. If unset, master is assumed.
-    """
-    git_branch: str
-
-    """
     Set to True if libcxx and cppstd can be ignored. This is useful for C libraries.
     """
     ignore_cpp_standard: bool = False
 
     def config_options(self):
-        if not self.git_branch:
-            self.git_branch = "master"
-
-        self.output.info("Repository: {}".format(self.git_repository))
-        self.output.info("Branch: {}".format(self.git_branch))
-
         if self.settings.os == "Windows":
             self.options.rm_safe("fPIC")
 
@@ -72,13 +55,8 @@ class AleyaConanBase:
             self.settings.rm_safe("compiler.libcxx")
             self.settings.rm_safe("compiler.cppstd")
 
-    def source(self):
-        git = Git(self, self.source_folder)
-        git.clone(url=self.git_repository, target='.')
-        git.checkout(commit=self.version)
-
     def layout(self):
-        self.folders.source = "src"
+        self.folders.source = "source"
         self.folders.build = "build"
 
     def build(self):
@@ -86,23 +64,6 @@ class AleyaConanBase:
 
     def generate(self):
         pass
-
-    def set_version(self):
-        # Enforce git describe version so that it also works
-        # when the provided version was a branch
-        self.version = self.version or self.__generate_version()
-
-    def __generate_version(self):
-        with TemporaryDirectory() as tmp_dir_name:
-            git = Git(self, tmp_dir_name)
-            git.clone(url=self.git_repository, target='.')
-            git.checkout(commit=self.git_branch)
-            result = self.__get_git_describe(git)
-            return result
-
-    @staticmethod
-    def __get_git_describe(git: Git):
-        return str(git.run("describe --tags")).lower()
 
 
 class AleyaCmakeBase(AleyaConanBase):
@@ -118,5 +79,5 @@ class AleyaCmakeBase(AleyaConanBase):
 
 class AleyaConanBaseConanFile(ConanFile):
     name = "aleya-conan-base"
-    version = "1.2.0"
+    version = "1.3.0"
     package_type = "python-require"
